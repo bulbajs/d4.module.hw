7cea57bfe90253d10f6ddf1c59264a3e015be75b
bulbajs <93475048+bulbajs@users.noreply.github.com>
2025-01-21 12:59:51 +0300

    filters.py edited (+widget)
    + Изменил файл добавив фильтр для времени публикации статьи через DateTimeFilter

--- d4_module_hw/news/filters.py
+++ d4_module_hw/news/filters.py
@@ -1,12 +1,13 @@
-from django_filters import FilterSet
+import django_filters
+from django_filters import FilterSet, DateTimeFilter, CharFilter
 from .models import Post
+from django import forms
 
 
 class PostFilter(FilterSet):
+    title = CharFilter(lookup_expr='icontains')
+    time_in_Post = DateTimeFilter(field_name='time_in_Post', lookup_expr='gte', widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+
     class Meta:
         model = Post
-        fields = {
-            'title': ['icontains'],
-            'categoryType': ['exact'],
-            'time_in_Post': ['gte', 'lte'],
-        }
+        fields = ['title', 'categoryType', 'time_in_Post']
